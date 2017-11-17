6693c0a4a2e339f8b6a23ab321db003ca5b945bb
Stolz <Stolz@users.noreply.github.com>
2017-11-17 17:18:50 +0800

    Added contract for Route storage

---
+++ lumen/app/Contracts/RouteRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Contracts;
+
+use App\Models\Route;
+
+interface RouteRepository
+{
+    /**
+     * Save a new route into the repository.
+     *
+     * @param  \App\Models\Route $route
+     * @return bool
+     */
+    public function create(Route $route): bool;
+
+    /**
+     * Update an existing route in the repository.
+     *
+     * @param  \App\Models\Route $route
+     * @return bool
+     */
+    public function update(Route $route): bool;
+
+    /**
+     * Retrieve a route by its token.
+     *
+     * @param  string $token
+     * @return \App\Models\Route|null
+     */
+    public function findByToken(string $token);
+}
